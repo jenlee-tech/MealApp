ac6455605f03c575deb810f4368450aef056eaf3
Jennifer Lee <jenlee.tech@gmail.com>
2022-02-14 08:19:12 -0500

    added a header component and css

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import classes from "./Header.module.css";
+import mealsImage from "../../components/Assets/meals.jpg";
+
+const Header = () => {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>Meals App</h1>
+      </header>
+      <div>
+        <img
+          src={mealsImage}
+          alt="A table of yummy food"
+          className={classes["main-image"]}
+        />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
