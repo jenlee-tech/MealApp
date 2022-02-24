5336de050b2a758c34d7ec98e95f708b3497cab5
Jennifer Lee <jenlee.tech@gmail.com>
2022-02-23 22:35:12 -0500

    added useRef in MealItemForm component

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,14 +1,14 @@
 import React from "react";
 import classes from "./Input.module.css";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input {...props.input} />
+      <input ref={ref} {...props.input} />
       {/* special React feature, only for inputs, it looks like deconstruction but not exactly, React converst, props.input(key:value) into key=value and places it as a HTML attribute feature */}
     </div>
   );
-};
+});
 
 export default Input;
