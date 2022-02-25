9f465e9b8786b26c31c49c7b0b4426f143ac4ec6
Jennifer Lee <jenlee.tech@gmail.com>
2022-02-25 01:08:04 -0500

    added the CartItem component for styling and rendering in the Cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import React from "react";
+import CartItem from "./CartItem";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
 import CartContext from "../../store/cart-context";
@@ -9,11 +10,20 @@ const Cart = (props) => {
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
+  const cartItemRemoveHandler = (id) => {};
 
+  const cartItemAddHandler = (item) => {};
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item, index) => (
-        <li key={item.index + Math.random()}>{item.name}</li>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
