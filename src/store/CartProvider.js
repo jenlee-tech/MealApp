5af5819d269fb8fde5dcb0215e2492b21686f1fa
Jennifer Lee <jenlee.tech@gmail.com>
2022-02-25 23:54:11 -0500

    added more logic for CareProvider

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,16 +8,37 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    const updatedItems = state.items.concat(action.item);
+    
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
-    return {
+   
+    const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id);
+    
+
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItem;
+    let updateItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount};
+        updatedItems = [...state.items];
+        updatedItems[existingCartItemIndex] = updatedItem;
+      } else {
+        
+        updateItems = state.items.concat(action.item);
+      }
+  }
+   
+ 
+  return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
     };
-  }
-  return defaultCartState;
-};
+}
+ return defaultCartState;
+}
 
 const CartProvider = (props) => {
   const [cartState, dispatchCartAction] = useReducer(
