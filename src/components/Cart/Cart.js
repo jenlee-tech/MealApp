8a7af453fdf4e849d96c811014ac885cc466c66b
Jennifer Lee <jenlee.tech@gmail.com>
2022-03-07 23:32:51 -0500

    added logic to remove items in the cart, plus or minus buttons

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -13,10 +13,13 @@ const Cart = (props) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`; //*use a template literal to get the dollar sign*//
   const hasItems = cartCtx.items.length > 0;
   const cartItemRemoveHandler = (id) => {
-    
+    cartCtx.removeItem(id);
   };
 
-  const cartItemAddHandler = (item) => {cartCtx.addItem({...item, amount: 1});
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item, index) => (
