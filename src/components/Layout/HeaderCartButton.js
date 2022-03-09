176d8b3fda2335409fcd977aca76c3e6f5e1ac8b
Jennifer Lee <jenlee.tech@gmail.com>
2022-03-08 23:03:12 -0500

    added useEffect and useState hooks into HeaderCartButton component

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,10 +1,11 @@
 import CartIcon from "../Cart/CartIcon"; //the actual image of a little whittle cart (svg)
 import React from "react";
 import classes from "./HeaderCartButton.module.css";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighLighted, setBtnIsHighLighted] = useState(false);
   const cartCtx = useContext(CartContext); //the useContext hook, has to use the object(CartContext)
 
   //using reduce method that uses the previous value and current value, check out MDN for fuller explanation on reduce method.
@@ -12,7 +13,15 @@ const HeaderCartButton = (props) => {
     return curNumber + item.amount;
   }, 0); //the context helps interject state data into components without the use of pop drilling.
 
-  const btnClasses = `${classes.button} ${classes.bump}`;
+  const { items } = cartCtx;
+
+  const btnClasses = `${classes.button} ${btnISHighLighted ? classes.bump : ''`;
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighLighted(true);
+  }, [items]);
 
   return (
     <button className={btnClasses} onClick={props.onShowCart}>
